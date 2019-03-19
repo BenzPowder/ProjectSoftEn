8f15cba54e0ff0946b9bd3f005e65f6c085e1f4a
Titaya <33252659+Titaya@users.noreply.github.com>
2019-03-19 22:55:39 +0700

    chang show class

--- taShowClass.php
+++ taShowClass.php
@@ -120,6 +120,7 @@ $i = 0;
                 </div>
                 <div class="container" align="center">
                     <a href="addSubject.php"><input type="button" name="addSubject" value="เพิ่มวิชา"> </a>
+                    <a href=""><input type="button" name="addTeacher" value="เพิ่มอาจารย์"> </a>
                 </div>
                 <!-- Scroll to Top Button (Only visible on small and extra-small screen sizes) -->
 
