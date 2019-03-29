cde50f2c0283474cd38d1bb469596af7e8aee4c7
Benz Boonsaree <33254030+BenzPowder@users.noreply.github.com>
2019-03-29 22:23:35 +0700

    Update teacherShowSectionDetail.php

--- teacherShowSectionDetail.php
+++ teacherShowSectionDetail.php
@@ -178,11 +178,6 @@ $i = 0;
     <!-- Custom scripts for this template -->
     <script src="js/freelancer.min.js"></script>
 
-    <div align="center">
-                <a href="teacherShowSection.php"><button type="button" class="btn btn-primary"
-                        id="addSubject">เปลี่ยน Section</button></a>
-            </div>
-
 </body>
 
 
